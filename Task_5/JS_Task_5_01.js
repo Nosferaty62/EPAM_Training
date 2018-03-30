39e8a1a1993533ed64ed1ca937bcb6cf7379a4ad
Dima <dmitry.ganin@gmail.com>
2018-03-30 18:54:59 +0300

    I have some question in getResl

--- Task_5/JS_Task_5_01.js
+++ Task_5/JS_Task_5_01.js
@@ -1,9 +1,9 @@
 var calculator = {
-  sum: function summary(a,c) {
-    return function(a,b) {
+  sum: (function summary(a) {
+    return function(b) {
       return a + b;
     };
-  },
+  })(),
   dif: function difference(a, b) {
     return a - b;
   },
@@ -12,6 +12,13 @@ var calculator = {
   },
   div: function divide(a, b) {
     return a / b;
+  },
+  res: function reset(){
+    return 0;
+  },
+  getResl: function getResult(){
+    
+    return calculator.mult(1,4);
   }
 };
 
@@ -19,4 +26,6 @@ console.log(calculator.sum(1,2));
 console.log(calculator.dif(1,2));
 console.log(calculator.mult(1,2));
 console.log(calculator.div(1,2));
+console.log(calculator.getResl());
+console.log(calculator.res(1,2));
 
